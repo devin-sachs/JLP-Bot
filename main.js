4771dcdaa93a3fa7bfcec9e88ec9593c23269567
devin-sachs <75508790+devin-sachs@users.noreply.github.com>
2025-06-16 19:08:45 -0700

    modify token to use environment variables instead of json.config, add start script to package.json

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+// const { token } = require('./config.json');
+const token = process.env.TOKEN;
 
 
 const client = new Client({
